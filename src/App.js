036d99c78540e6d4523e91c82fd02c6c1b39727e
Ben <parishben13@gmail.com>
2021-08-12 22:47:18 -0400

    finished up a working timer. Takes User input of a future date & then renders year/month/day/min/sec countdown. Utilizing React useState/useEffect for running continuously via setInterval function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Timer from './timer.js';
+import OtherTimer from './otherTimer';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,8 +18,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
       <Timer />
+      {/* <OtherTimer/> */}
     </div>
   );
 }
--- src/timer.js
+++ src/timer.js
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from "react";
 
 
 const futureDate = new Date(2050, 0, 1);
+
+
 const getDateDiff = (date1, date2) => {
   const diff = new Date(date2.getTime() - date1.getTime());
+  //console.log(diff.getTime())
   return {
     year: diff.getUTCFullYear() - 1970,
     month: diff.getUTCMonth(),
@@ -13,6 +16,7 @@ const getDateDiff = (date1, date2) => {
     second: diff.getUTCSeconds()
   };
 };
+
 const formatDate = (date) => {
   let d = new Date(date),
     month = (d.getMonth() + 1).toString(),
@@ -23,22 +27,52 @@ const formatDate = (date) => {
   return [year, month, day].join("-");
 };
 
+var dasher = undefined;
+var stasher = undefined;
+
+
 
 export default function Timer() {
+
+ 
+  const preventdef = (e) => {
+    e.preventDefault()
+    let dateinp = dasher.split('-')
+    if (dateinp[1].charAt(0)=='0'){
+      dateinp[1] = dateinp[1].charAt(1)
+    }
+    if (dateinp[2].charAt(0)=='0'){
+      dateinp[2] = dateinp[2].charAt(1)
+    }
+    dasher = dateinp.join("-")
+    stasher = new Date(dasher)
+    console.log( dasher, stasher)
+    dasher = ''
+  }
+  const uptodash = (e) => {
+    //console.log(e)
+    dasher = e.target.value
+  }
   const [diff, setDiff] = useState({});
+
   useEffect(() => {
     const timer = setInterval(() => {
-      setDiff(getDateDiff(new Date(), futureDate));
+      stasher && setDiff(getDateDiff(new Date(), stasher));
     }, 1000);
     return () => clearInterval(timer);
   }, []);
+
   return (
-    <div className="App">
-      <p style={{padding:'0px'}}>
-        <span style={{border:'2pt solid blue', 'border-radius':'10pt', padding:'2pt', paddingTop:'0pt'}}>{diff.year} years, {diff.month} months, {diff.day} days,
-        {diff.hour} hours, {diff.minute} minute, {diff.second} seconds until{" "}
-        {formatDate(futureDate)}
-      </span></p>
+    <div className="BApp">
+      <form onSubmit={(e) => {preventdef(e)}}>
+        <label>Enter a Future Date (YYYY-MM-DD) To Start the Countdown!</label>
+        <input type="text" onChange={(e)=> uptodash(e)} name="dateinput"  value={dasher}/>
+      </form>
+       { !diff['second'] == '' ? <p style={{padding:'0px'}}>
+         <span style={{border:'2pt solid blue', 'border-radius':'10pt', padding:'2pt', paddingTop:'0pt'}}>{diff && diff.year} years, {diff && diff.month} months, {diff && diff.day} days,
+        {diff && diff.hour} hours, {diff && diff.minute} minute, {diff && diff.second} seconds until{" "}
+        {stasher && formatDate(stasher)}
+      </span></p> : <p>Waiting For Input...</p>}
     </div>
   );
 }
